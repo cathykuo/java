891582fc888b6590ee67c1930a234295e446dd2c
Cathy Kuo <cathykuo@Cathys-MacBook-Pro.local>
2019-06-16 17:31:31 -0700

    My First File

---
+++ MySweetWorld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MySweetWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+		System.out.println("What is your name?");
+	
+	}
+}
+ 
